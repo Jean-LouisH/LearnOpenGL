50aba9412cc79eda4f3e6e4a365be2a40ebdc9bd
Jean-LouisH <LouisLou512.e@gmail.com>
2020-01-04 14:31:17 -0500

    Defined the 2D triangle where 3D coordinates are still referenced but ignored with z values of 0.0f

--- OpenGL_Training/Exercises/Triangle.cpp
+++ OpenGL_Training/Exercises/Triangle.cpp
@@ -4,6 +4,16 @@ void OpenGL_Training::Exercises::triangle()
 {
 	ApplicationWindow appWindow = ApplicationWindow("Triangle");
 
+	//Although a 2D shape, the triangle's z-axis is 
+	// still accounted for in GL functions
+	float triangleVertices[] = 
+	{
+	//   x      y     z
+		-0.5f, -0.5f, 0.0f, //lower left point
+		 0.5f, -0.5f, 0.0f, //lower right point
+		 0.0f,  0.5f, 0.0f  //top middle point
+	};
+
 	while (appWindow.handleEvents())
 	{
 		appWindow.swapBuffers();
