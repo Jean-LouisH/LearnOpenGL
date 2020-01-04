cb123fcdae317d4203fa68412e122a23bfa735b0
Jean-LouisH <LouisLou512.e@gmail.com>
2020-01-04 13:04:31 -0500

    Moved Blank() glClear operation outside of the main loop to draw the colour statically

--- OpenGL_Training/Exercises/Blank.cpp
+++ OpenGL_Training/Exercises/Blank.cpp
@@ -4,10 +4,11 @@ void OpenGL_Training::Exercises::blank()
 {
 	ApplicationWindow appWindow = ApplicationWindow("Blank");
 
+	glClearColor(0.5f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
 	while (appWindow.handleEvents())
 	{
-		glClearColor(0.5f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
 		appWindow.swapBuffers();
 	}
 }
