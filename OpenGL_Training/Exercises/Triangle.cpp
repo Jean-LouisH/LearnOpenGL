062ae5c5b1f446832b1fdad94359cd5ba60f15da
Jean-LouisH <LouisLou512.e@gmail.com>
2020-01-04 15:07:02 -0500

    Generated and buffered triangle vertex data

--- OpenGL_Training/Exercises/Triangle.cpp
+++ OpenGL_Training/Exercises/Triangle.cpp
@@ -8,11 +8,17 @@ void OpenGL_Training::Exercises::triangle()
 	// still accounted for in GL functions
 	float triangleVertices[] = 
 	{
-	//   x      y     z
+		//x      y     z
 		-0.5f, -0.5f, 0.0f, //lower left point
 		 0.5f, -0.5f, 0.0f, //lower right point
 		 0.0f,  0.5f, 0.0f  //top middle point
 	};
+
+	unsigned int vertexBufferObject;
+	glGenBuffers(1, &vertexBufferObject);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
+
 
 	while (appWindow.handleEvents())
 	{
