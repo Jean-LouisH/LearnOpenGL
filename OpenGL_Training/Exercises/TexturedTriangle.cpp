b0062a22a0acb660cc3cde28738da042b1df1e79
Jean-LouisH <LouisLou512.e@gmail.com>
2020-01-14 11:52:11 -0500

    Created GL texture object, applied texture filtering parameters, loaded and copied container JPG to GPU with mipmaps

--- OpenGL_Training/Exercises/TexturedTriangle.cpp
+++ OpenGL_Training/Exercises/TexturedTriangle.cpp
@@ -17,11 +17,28 @@ void OpenGL_Training::Exercises::texturedTriangle()
 		0.5f, 1.0f   // top-center corner
 	};
 
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
 
-	//float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
-	//glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	SDL_Surface* surface = IMG_Load("../Textures/container.jpg");
+
+	if (surface->pixels != NULL)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, surface->w, surface->h, 0, GL_RGB, GL_UNSIGNED_BYTE, surface->pixels);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	SDL_FreeSurface(surface);
 
 	while (appWindow.handleEvents())
 	{
